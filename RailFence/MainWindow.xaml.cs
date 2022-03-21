d043c30ba004a076ae5ef3bacf40df9fb1225672
konrad121970 <konrad121970@windowslive.com>
2022-03-21 20:32:12 +0100

    Dodano zadanie 1 - Rail Fence

---
+++ RailFence/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RailFence
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        // RailFence rail = new RailFence();
+        public string inputData;
+        public int n;
+        public string outputData;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Szyfruj_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(toCipher.Text.Length == 0 || nTextBox.Text.Length == 0)
+            {
+                cipheredText.Text = "Podaj właściwe dane";
+
+                return;
+            }
+
+            inputData = toCipher.Text;
+            n = int.Parse(nTextBox.Text);
+
+            RailFenceMachine rail = new RailFenceMachine(n);
+
+            outputData = rail.Encrypt(inputData);
+            cipheredText.Text = outputData;
+        }
+
+        private void Odszyfruj_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (toDecipher.Text.Length == 0 || nTextBox.Text.Length == 0)
+            {
+                decipheredText.Text = "Podaj właściwe dane";
+
+                return;
+            }
+
+            inputData = toDecipher.Text;
+            n = int.Parse(nTextBox2.Text);
+
+            RailFenceMachine rail = new RailFenceMachine(n);
+
+            outputData = rail.Decrypt(inputData);
+            decipheredText.Text = outputData;
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void toDecipherText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ RailFence/RailFenceMachine.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RailFence
+{
+    class RailFenceMachine
+    {
+        public int rails { get; set; }
+
+        public RailFenceMachine(int _n)
+        {
+            rails = _n; // Ilość szyn
+        }
+
+        /*
+         * Szyfrowanie
+         */
+        public string Encrypt(string textToCipher)
+        {
+            StringBuilder tab = new StringBuilder(); // Tablica do tworzenia tekstu po szyfrowaniu
+
+            for (int i = 0; i < rails; i++)
+            {
+                for (int j = 0; j < Math.Ceiling((decimal)textToCipher.Length / rails); j++)
+                {
+                    if (i == 0)
+                    {
+                        int index;
+                        index = (2 * (rails - 1) * j); // Powtarzalność sekwencji góra-dół = 2(n-1)
+                        if (index < textToCipher.Length)    // Jeżeli index nie wychodzi poza granicę rozmiaru tablicy
+                        {
+                            tab.Append(textToCipher[index]);
+                        }
+
+                    }
+                    else if (i == rails - 1)    // Jeżeli dojdziemy do dołu płotka
+                    {
+                        int index;
+                        index = ( 2 * (rails - 1) * j) + rails - 1;
+                        if (index < textToCipher.Length)
+                        {
+                            tab.Append(textToCipher[index]);
+                        }
+                    }
+                    else
+                    {
+                        int top;
+
+                        top = ((2 * (rails - 1)) * j);
+                        int index;
+                        index = top - i;
+                        if (index > 0 && index < textToCipher.Length)
+                        {
+                            tab.Append(textToCipher[index]);
+                        }
+                        index = top + i;
+                        if (index > 0 && index < textToCipher.Length)
+                        {
+                            tab.Append(textToCipher[index]);
+                        }
+
+                    }
+                }
+            }
+
+            return tab.ToString();
+        }
+
+        /*
+         * Deszyfrowanie
+         */
+        public string Decrypt(string textToDecipher)
+        {
+            char[] tmp = new char[textToDecipher.Length];
+            int pom = 0;
+            for (int i = 0; i < rails; i++)
+            {
+                for (int j = 0; j < Math.Ceiling((decimal)textToDecipher.Length / rails); j++)
+                {
+                    if (i == 0)
+                    {
+                        int index;
+                        index = ((2 * (rails - 1)) * j);
+
+                        if (index < textToDecipher.Length)
+                        {
+                            tmp[index] = textToDecipher[pom];
+                            pom++;
+                        }
+
+                    }
+                    else if (i == rails - 1)
+                    {
+                        int index;
+                        index = ((2 * (rails - 1)) * j) + rails - 1;
+                        if (index < textToDecipher.Length)
+                        {
+                            tmp[index] = textToDecipher[pom];
+                            pom++;
+                        }
+                    }
+                    else
+                    {
+                        int top;
+
+                        top = ((2 * (rails - 1)) * j);
+                        int index;
+                        index = top - i;
+                        if (index > 0 && index < textToDecipher.Length)
+                        {
+                            tmp[index] = textToDecipher[pom];
+                            pom++;
+                        }
+                        index = top + i;
+                        if (index > 0 && index < textToDecipher.Length)
+                        {
+                            tmp[index] = textToDecipher[pom];
+                            pom++;
+                        }
+
+                    }
+                }
+            }
+
+            return new string(tmp);
+        }
+    }
+}
